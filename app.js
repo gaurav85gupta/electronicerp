fbbfce09319480baacafb26a097e2b4dd9f967c8
gaurav85gupta <145853396+gaurav85gupta@users.noreply.github.com>
2026-07-20 12:58:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://electronicerp-1.onrender.com';
 
 async function checkServerHealth() {
   const statusElement = document.getElementById('status-message');
